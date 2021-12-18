4cfa8532be9ed17bd5f5daa9e14b169bab67cd0a
Nikolay Ulmasov <nikolay.ulmasov@databricks.com>
2021-12-18 20:31:36 +0000

    really fix timing

--- python/examples/my-detection.py
+++ python/examples/my-detection.py
@@ -11,7 +11,7 @@ net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.6)
 camera = jetson.utils.videoSource("/dev/video0") 
 display = jetson.utils.videoOutput("display://0")
 
-start = dt.datetime.now()
+checkpoint = dt.datetime.now()
 i = 0
 while display.IsStreaming():
 	img = camera.Capture()
@@ -20,9 +20,9 @@ while display.IsStreaming():
 	fps = net.GetNetworkFPS()
 	display.SetStatus("Object Detection | {:.0f} FPS".format(fps))
 	if i % 100 == 0:
-		seconds = dt.datetime.now() - start
-		if  s > 30:
-			h, rem = divmod(s, 3600)
+		seconds = dt.datetime.now() - checkpoint
+		if  seconds > 30:
+			h, rem = divmod(seconds, 3600)
 			m, s = divmod(rem, 60)
 			if h > 0:
 				msg = '{:02} hours {:02} minutes {:02} seconds'.format(int(h), int(m), int(s))
@@ -31,6 +31,8 @@ while display.IsStreaming():
 			else:
 				msg = '{:02} seconds'.format(int(s))
 			print(f'- running for: {msg}, Current FPS: {fps})')
-		i = 0
+
+			checkpoint = dt.datetime.now()
+			i = 0
 	i += 1
 
